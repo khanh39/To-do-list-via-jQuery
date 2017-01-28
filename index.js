3fbb7e5b0d07e9c07845b41bf0688c22152ed3c3
Hamza Khan <khanh39@mcmaster.ca>
2017-01-28 18:36:31 -0500

    added comments in the document files to make code easier to understand for the viewer

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 $(function() {
    $("#newEl").on("click",
    function(){
-   var inputText= $("input").val();
+   var inputText= $("input").val();		//Assigns user input to a variaBle
 
-   if(inputText == "")
+   if(inputText == "")		//Ensure that the user entered something before adding a task
    {alert("Invalid item name");}
    else
    {
-     var el=  $("<li></li>").text(inputText);
-     $(el).append("<button class='removeElement'>Remove</button>");
+     var el=  $("<li></li>").text(inputText);		//Creates a new list element with the user's input
+     $(el).append("<button class='removeElement'>Remove</button>");		//Adds the new list element to the end of the task list
 
      $("#list").append(el);
             $("input").val("");
-            $(".removeElement").on("click", function() {                $(this).parent().remove();
+            $(".removeElement").on("click", function() {                $(this).parent().remove();		//Removes a task from the list by referencing its list element from the parent list
    })
 }
 })
